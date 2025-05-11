71563504cb6093f6cd93c7e96daa7e920e0a5c87
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-05-11 00:58:22 -0700

    Update Matrices.cpp

--- code/Matrices.cpp
+++ code/Matrices.cpp
@@ -102,7 +102,7 @@ namespace Matrices
         return os;
     }
 
-
+//Done by Anna
     RotationMatrix::RotationMatrix(double theta) : Matrix(2,2){
         a[0][0] = cos(theta);
         a[0][1] = -sin(theta);
